15ea28addf7f08a0cd0be2d501c3e8811dde375a
Ana-Morales <moralesanaruth@gmail.com>
2020-12-08 14:31:17 +0000

    First commit

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Make changes to configuration file
-file_line { '/etc/ssh/ssh_config':
-  ensure => 'present',
+file_line { 'authenticate without a pass':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
-file_line { '/etc/ssh/ssh_config':
-  ensure => 'present',
+file_line { 'identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton',
 }
