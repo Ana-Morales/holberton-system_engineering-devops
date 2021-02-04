a84eafc403444da8c86d7a1b88b2eb1d7accf0af
Ana-Morales <moralesanaruth@gmail.com>
2021-02-04 19:44:26 +0000

    Change order

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,18 +5,18 @@ given subreddit"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=""):
     """List containing the titles of all hot articles for a given subreddit"""
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     r = requests.get(url, headers={'user-agent': 'projectapi 0.1'},
                      params={'after': after})
-    after = r.json().get('data').get('after')
 
     if after is None:
         return hot_list
     if r.status_code == 200:
         r = r.json()
+        after = r.get('data').get('after')
         hot = r.get('data').get('children')
         for child in hot:
             hot_list.append(child.get('data').get('title'))
